cb12aa1ec3718fd752ea3b5d23384269f5339e00
Prajjwal Sharma <prajjwal.sharma@paynways.com>
2024-10-12 11:24:17 +0530

    added

---
+++ trunk/business_logic_ui_api/src/main/java/com/example/business/BusinessLogicUiApiApplication.java
@@ -0,0 +1,19 @@
+package com.example.business;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@AutoConfiguration
+@SpringBootApplication
+public class BusinessLogicUiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessLogicUiApiApplication.class, args);
+		System.out.println("jgg");
+		
+		
+		
+		
+	}
+
+}
