dcefe0a5f9038644ab5cca8a6afc2a05997cea96
Prajjwal Sharma <prajjwal.sharma@paynways.com>
2024-10-12 10:38:11 +0530

    git psu

---
+++ Business_logic/src/test/java/com/example/JavaComplete/JavaCompleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaComplete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCompleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
