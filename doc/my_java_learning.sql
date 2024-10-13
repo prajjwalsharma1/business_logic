92efb15ca0508ba949fced64e2a4a6d3169d263e
Prajjwal Sharma <prajjwal.sharma@paynways.com>
2024-10-13 15:47:06 +0530

    added

---
+++ doc/my_java_learning.sql
@@ -0,0 +1,36 @@
+ALTER SESSION SET CURRENT_SCHEMA="RPS_ADMIN"; 
+update  tb_rp_m_usr  set lock_ts=null,usr_state='ACTIVE',FLD_LGN_CNT=0;
+commit;
+Applets are intended to be small programs. They are typically used to display
+data provided by the server, handle user input, or provide simple functions, such as a loan
+calculator, that execute locally rather than on the server
+
+Because servlets (like all Java programs) are compiled into bytecode and executed
+by the JVM, they are highly portable. Thus, the same servlet can be used in a variety of
+different server environments. The only requirements are that the server support the JVM
+and a servlet container
+• Simple
+• Secure
+• Portable
+• Object-oriented
+• Robust
+• Multithreaded
+• Architecture-neutral
+• Interpreted
+• High performance
+• Distributed
+• Dynamic
+
+To better understand how Java is robust, consider two of the main reasons for program
+failure: memory management mistakes and mishandled exceptional conditions (that is,
+run-time errors).
+
+
+just a name is a memory allocation of the varibale  so it use the memory 
+
+all tracedecial function return the double VALUE
+The range of a char is 0 to 65,536. There are no negative chars
+
+boolean always work for true and false value
+
+
